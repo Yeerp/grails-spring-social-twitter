54d31101afc50d5bb7da679c1b47c11020d481cd
Michael <michael.astreiko@gmail.com>
2015-02-02 11:49:51 +0300

    Update BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,6 +19,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.mavenCache="/usr/local/nearme_portal/builds/"
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
   inherits("global") {
